48044ac04a106e83532800c5844254e6b9f7566f
KANI <289121533+ka9i@users.noreply.github.com>
2026-06-02 12:28:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import preact from "@preact/preset-vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "/course/iot-cohort-1",
+  base: "/course/iot-cohort-1/",
 });
